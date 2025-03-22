96a4f0604cd30af48ac4952a5f785f4a50a0a0b4
tibrisch <torbjorn.svangard@gmail.com>
2025-03-22 09:46:13 +0100

    Refactor generator.py and enable MIMI decoder streaming.

--- generator.py
+++ generator.py
@@ -30,56 +30,42 @@ def load_llama3_tokenizer():
     tokenizer._tokenizer.post_processor = TemplateProcessing(
         single=f"{bos}:0 $A:0 {eos}:0",
         pair=f"{bos}:0 $A:0 {eos}:0 {bos}:1 $B:1 {eos}:1",
-        special_tokens=[(f"{bos}", tokenizer.bos_token_id), (f"{eos}", tokenizer.eos_token_id)],
+        special_tokens=[
+            (f"{bos}", tokenizer.bos_token_id),
+            (f"{eos}", tokenizer.eos_token_id),
+        ],
     )
-
     return tokenizer
 
 
 class Generator:
-    def __init__(
-        self,
-        model: Model,
-    ):
+    def __init__(self, model: Model):
         self._model = model
         self._model.setup_caches(1)
-
         self._text_tokenizer = load_llama3_tokenizer()
+        self.device = next(model.parameters()).device
+        self._audio_tokenizer = self._load_audio_tokenizer()
+        self._watermarker = load_watermarker(device=self.device)
+        self.sample_rate = self._audio_tokenizer.sample_rate
 
-        device = next(model.parameters()).device
+    def _load_audio_tokenizer(self):
         mimi_weight = hf_hub_download(loaders.DEFAULT_REPO, loaders.MIMI_NAME)
-        mimi = loaders.get_mimi(mimi_weight, device=device)
+        mimi = loaders.get_mimi(mimi_weight, device=self.device)
         mimi.set_num_codebooks(32)
-        self._audio_tokenizer = mimi
-
-        self._watermarker = load_watermarker(device=device)
-
-        self.sample_rate = mimi.sample_rate
-        self.device = device
+        return mimi
 
     def _tokenize_text_segment(self, text: str, speaker: int) -> Tuple[torch.Tensor, torch.Tensor]:
-        frame_tokens = []
-        frame_masks = []
-
         text_tokens = self._text_tokenizer.encode(f"[{speaker}]{text}")
         text_frame = torch.zeros(len(text_tokens), 33).long()
         text_frame_mask = torch.zeros(len(text_tokens), 33).bool()
         text_frame[:, -1] = torch.tensor(text_tokens)
         text_frame_mask[:, -1] = True
-
-        frame_tokens.append(text_frame.to(self.device))
-        frame_masks.append(text_frame_mask.to(self.device))
-
-        return torch.cat(frame_tokens, dim=0), torch.cat(frame_masks, dim=0)
+        return text_frame.to(self.device), text_frame_mask.to(self.device)
 
     def _tokenize_audio(self, audio: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-        frame_tokens = []
-        frame_masks = []
-
-        # (K, T)
         audio = audio.to(self.device)
         audio_tokens = self._audio_tokenizer.encode(audio.unsqueeze(0).unsqueeze(0))[0]
-        # add EOS frame
+
         eos_frame = torch.zeros(audio_tokens.size(0), 1).to(self.device)
         audio_tokens = torch.cat([audio_tokens, eos_frame], dim=1)
 
@@ -88,20 +74,49 @@ class Generator:
         audio_frame[:, :-1] = audio_tokens.transpose(0, 1)
         audio_frame_mask[:, :-1] = True
 
-        frame_tokens.append(audio_frame)
-        frame_masks.append(audio_frame_mask)
-
-        return torch.cat(frame_tokens, dim=0), torch.cat(frame_masks, dim=0)
+        return audio_frame, audio_frame_mask
 
     def _tokenize_segment(self, segment: Segment) -> Tuple[torch.Tensor, torch.Tensor]:
-        """
-        Returns:
-            (seq_len, 33), (seq_len, 33)
-        """
         text_tokens, text_masks = self._tokenize_text_segment(segment.text, segment.speaker)
         audio_tokens, audio_masks = self._tokenize_audio(segment.audio)
-
         return torch.cat([text_tokens, audio_tokens], dim=0), torch.cat([text_masks, audio_masks], dim=0)
+
+    def _prepare_prompt_tokens(self, text: str, speaker: int, context: List[Segment]):
+        tokens, tokens_mask = zip(*[self._tokenize_segment(seg) for seg in context]) if context else ([], [])
+        gen_tokens, gen_masks = self._tokenize_text_segment(text, speaker)
+        return (
+            torch.cat([*tokens, gen_tokens], dim=0).long().to(self.device),
+            torch.cat([*tokens_mask, gen_masks], dim=0).bool().to(self.device),
+        )
+
+    def generate_stream(
+            self,
+            text: str,
+            speaker: int,
+            context: List[Segment],
+            max_audio_length_ms=90_000,
+            temperature=0.9,
+            topk=50):
+        self._model.reset_caches()
+        max_audio_frames = max_audio_length_ms // 80
+        prompt_tokens, prompt_tokens_mask = self._prepare_prompt_tokens(text, speaker, context)
+
+        curr_tokens, curr_tokens_mask = prompt_tokens.unsqueeze(0), prompt_tokens_mask.unsqueeze(0)
+        curr_pos = torch.arange(0, prompt_tokens.size(0)).unsqueeze(0).long().to(self.device)
+        max_seq_len = 2048 - max_audio_frames
+
+        if curr_tokens.size(1) >= max_seq_len:
+            raise ValueError(f"Inputs too long, must be below {max_seq_len}")
+
+        with self._audio_tokenizer.streaming(1):
+            for _ in range(max_audio_frames):
+                sample = self._model.generate_frame(curr_tokens, curr_tokens_mask, curr_pos, temperature, topk)
+                if torch.all(sample == 0):
+                    break  # EOS
+                yield self._audio_tokenizer.decode(sample.unsqueeze(-1)).squeeze()
+                curr_tokens = torch.cat([sample, torch.zeros(1, 1).long().to(self.device)], dim=1).unsqueeze(1)
+                curr_tokens_mask = torch.cat([torch.ones_like(sample).bool(), torch.zeros(1, 1).bool().to(self.device)], dim=1).unsqueeze(1)
+                curr_pos = curr_pos[:, -1:] + 1
 
     @torch.inference_mode()
     def generate(
@@ -109,63 +124,29 @@ class Generator:
         text: str,
         speaker: int,
         context: List[Segment],
-        max_audio_length_ms: float = 90_000,
-        temperature: float = 0.9,
-        topk: int = 50,
-    ) -> torch.Tensor:
-        self._model.reset_caches()
+        max_audio_length_ms=90_000,
+        temperature=0.9,
+        topk=50
+        ) -> torch.Tensor:
 
-        max_audio_frames = int(max_audio_length_ms / 80)
-        tokens, tokens_mask = [], []
-        for segment in context:
-            segment_tokens, segment_tokens_mask = self._tokenize_segment(segment)
-            tokens.append(segment_tokens)
-            tokens_mask.append(segment_tokens_mask)
-
-        gen_segment_tokens, gen_segment_tokens_mask = self._tokenize_text_segment(text, speaker)
-        tokens.append(gen_segment_tokens)
-        tokens_mask.append(gen_segment_tokens_mask)
-
-        prompt_tokens = torch.cat(tokens, dim=0).long().to(self.device)
-        prompt_tokens_mask = torch.cat(tokens_mask, dim=0).bool().to(self.device)
-
-        samples = []
-        curr_tokens = prompt_tokens.unsqueeze(0)
-        curr_tokens_mask = prompt_tokens_mask.unsqueeze(0)
-        curr_pos = torch.arange(0, prompt_tokens.size(0)).unsqueeze(0).long().to(self.device)
-
-        max_seq_len = 2048 - max_audio_frames
-        if curr_tokens.size(1) >= max_seq_len:
-            raise ValueError(f"Inputs too long, must be below max_seq_len - max_audio_frames: {max_seq_len}")
-
-        for _ in range(max_audio_frames):
-            sample = self._model.generate_frame(curr_tokens, curr_tokens_mask, curr_pos, temperature, topk)
-            if torch.all(sample == 0):
-                break  # eos
-
-            samples.append(sample)
-
-            curr_tokens = torch.cat([sample, torch.zeros(1, 1).long().to(self.device)], dim=1).unsqueeze(1)
-            curr_tokens_mask = torch.cat(
-                [torch.ones_like(sample).bool(), torch.zeros(1, 1).bool().to(self.device)], dim=1
-            ).unsqueeze(1)
-            curr_pos = curr_pos[:, -1:] + 1
-
-        audio = self._audio_tokenizer.decode(torch.stack(samples).permute(1, 2, 0)).squeeze(0).squeeze(0)
+        samples = self.generate_stream(
+            text,
+            speaker,
+            context,
+            max_audio_length_ms,
+            temperature,
+            topk)
+        
+        audio = torch.cat(list(samples))
 
         # This applies an imperceptible watermark to identify audio as AI-generated.
         # Watermarking ensures transparency, dissuades misuse, and enables traceability.
         # Please be a responsible AI citizen and keep the watermarking in place.
         # If using CSM 1B in another application, use your own private key and keep it secret.
         audio, wm_sample_rate = watermark(self._watermarker, audio, self.sample_rate, CSM_1B_GH_WATERMARK)
-        audio = torchaudio.functional.resample(audio, orig_freq=wm_sample_rate, new_freq=self.sample_rate)
-
-        return audio
+        return torchaudio.functional.resample(audio, orig_freq=wm_sample_rate, new_freq=self.sample_rate)
 
 
 def load_csm_1b(device: str = "cuda") -> Generator:
-    model = Model.from_pretrained("sesame/csm-1b")
-    model.to(device=device, dtype=torch.bfloat16)
-
-    generator = Generator(model)
-    return generator
+    model = Model.from_pretrained("sesame/csm-1b").to(device=device, dtype=torch.bfloat16)
+    return Generator(model)
